288cac6ee9c98e9b64d00e0bf909311f7bcebdd3
itscsp <chethanspoojary1@gmail.com>
2024-12-18 08:17:21 +0530

    Input and Output decorator

--- 01-essentials/src/app/user/user.component.ts
+++ 01-essentials/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import { Component, EventEmitter, Input, Output} from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -12,14 +12,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length )
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({required:true}) id!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter();
 
   get imagePath() {
     return 'assets/users/'+this.avatar
   }
 
   onSelectUser() {
-   
+   this.select.emit(this.id)
   }
 }
