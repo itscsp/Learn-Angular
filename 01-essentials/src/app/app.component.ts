f0584bd559004d1101660561466622f5c8b8391f
itscsp <chethanspoojary1@gmail.com>
2024-12-18 08:48:39 +0530

    Pratice excerise in Angular component data passing

--- 01-essentials/src/app/app.component.ts
+++ 01-essentials/src/app/app.component.ts
@@ -2,18 +2,21 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = DUMMY_USERS[0].id;
 
   onSelectUser(id:string){
+    this.selectedUserId = id
     console.log("Selected User with ID:", id)
   }
 }
