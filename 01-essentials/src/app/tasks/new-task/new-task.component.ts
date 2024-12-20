d96b962292a23b75e09ff563c6067a1dd2fd7efd
itscsp <chethanspoojary1@gmail.com>
2024-12-21 00:12:52 +0530

    Handing form in Angualr

--- 01-essentials/src/app/tasks/new-task/new-task.component.ts
+++ 01-essentials/src/app/tasks/new-task/new-task.component.ts
@@ -1,14 +1,22 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<void>()
+  enteredTitle = '';
+  enteredSummry = "";
+  enteredDate = ""; 
+
+  onSubmit() {
+    console.log(this.enteredTitle, this.enteredSummry, this.enteredDate)
+  }
 
   onCancel() {
     this.cancel.emit()
