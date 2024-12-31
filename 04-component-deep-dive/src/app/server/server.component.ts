cef91a2704c0eb40c212666cfc0bcdd582220acd
itscsp <chethanspoojary1@gmail.com>
2024-12-31 11:29:55 +0530

    spliing p

---
+++ 04-component-deep-dive/src/app/server/server.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  standalone: true,
+  imports: [],
+  templateUrl: './server.component.html',
+  styleUrl: './server.component.css'
+})
+export class ServerComponent {
+
+  @Input() currentStatus?: string = "";
+
+}
