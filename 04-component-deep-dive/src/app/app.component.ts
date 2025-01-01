0e924e9c66be3525072eb1f98fa72b57bfa10732
Chethan S Poojary <86185190+itscsp@users.noreply.github.com>
2025-01-01 09:54:21 +0000

    Adding bashboard item wrapper

--- 04-component-deep-dive/src/app/app.component.ts
+++ 04-component-deep-dive/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
+import { TicketsComponent } from './dashboard/tickets/tickets.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, DashboardItemComponent, TicketsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
