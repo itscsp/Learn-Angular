eb34c141b8f2ffe5a81ce3044425df61cf1a6a16
Chethan S Poojary <86185190+itscsp@users.noreply.github.com>
2025-02-13 17:46:25 +0000

    Implementing API into app

--- 11-http-requests/src/app/places/available-places/available-places.component.ts
+++ 11-http-requests/src/app/places/available-places/available-places.component.ts
@@ -5,6 +5,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { catchError, map, throwError } from 'rxjs';
+import { PlacesService } from '../places.service';
 
 @Component({
   selector: 'app-available-places',
@@ -19,22 +20,15 @@ export class AvailablePlacesComponent implements OnInit {
   isFetching = signal(false)
   error = signal('')
   private httpClient = inject(HttpClient);
-  private destroyRef = inject(DestroyRef)
+  private destroyRef = inject(DestroyRef);
+  private placesService = inject(PlacesService)
+  
 
   // constructor(private httpClient: HttpClient){}
 
   ngOnInit(){
     this.isFetching.set(true);
-    const subscription = this.httpClient.get<{places: Place[]}>('http://127.0.0.1:3000/places')
-    .pipe(
-      map((resData) => resData.places),
-      catchError((error) => {
-        console.log(error)
-          return throwError(() => {
-            return new Error('Some thing went wrong.')
-          });
-        })
-      )
+    const subscription = this.placesService.loadAvailablePlaces()
     .subscribe({
       next: (places) => {
         this.places.set(places);
@@ -52,5 +46,15 @@ export class AvailablePlacesComponent implements OnInit {
       subscription.unsubscribe();
     });
 
+  }
+
+  onSelectPlace(selectedPlace: Place) {
+    const subscription = this.placesService.addPlaceToUserPlaces(selectedPlace).subscribe({
+      next: (resData) => console.log(resData)
+    })
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
   }
 }
--- 11-http-requests/src/app/places/places.service.ts
+++ 11-http-requests/src/app/places/places.service.ts
@@ -1,20 +1,75 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 
 import { Place } from './place.model';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, tap, throwError } from 'rxjs';
+import { ErrorService } from '../shared/error.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlacesService {
+  private errorService = inject(ErrorService)
+  private httpClient = inject(HttpClient);
   private userPlaces = signal<Place[]>([]);
+
 
   loadedUserPlaces = this.userPlaces.asReadonly();
 
-  loadAvailablePlaces() {}
+  loadAvailablePlaces() {
+    return this.fetchPlaces('http://127.0.0.1:3000/places', 'Some thing went wrong.')
+  }
 
-  loadUserPlaces() {}
+  loadUserPlaces() {
+    return this.fetchPlaces('http://127.0.0.1:3000/user-places', 'Some thing went wrong.')
+    .pipe(tap({
+      next: (userPlaces) => this.userPlaces.set(userPlaces)}))
+  }
 
-  addPlaceToUserPlaces(place: Place) {}
+  addPlaceToUserPlaces(place: Place) {
+    const prevPlaces = this.userPlaces();
 
-  removeUserPlace(place: Place) {}
+    if(!prevPlaces.some((p) => p.id === place.id)){
+      this.userPlaces.set([...prevPlaces, place])
+    }
+    
+    return  this.httpClient.put('http://127.0.0.1:3000/user-places', {
+      placeId: place.id
+    }).pipe(
+      catchError(error => {
+        this.userPlaces.set(prevPlaces);
+        this.errorService.showError('Failed to store selected place')
+        return throwError(() => new Error('Failed to store selected place'))
+      })
+    )
+  }
+
+  removeUserPlace(place: Place) {
+    const prevPlaces = this.userPlaces();
+
+    if(prevPlaces.some((p) => p.id === place.id)){
+      this.userPlaces.set(prevPlaces.filter(p => p.id !== place.id)) 
+    }
+
+    return this.httpClient.delete('http://127.0.0.1:3000/user-places/' + place.id).pipe(
+      catchError(error => {
+        this.userPlaces.set(prevPlaces);
+        this.errorService.showError('Failed to remove selected place')
+        return throwError(() => new Error('Failed to remove selected place'))
+      })
+    )
+  }
+
+  private fetchPlaces(url:string, errorMessage: string) {
+    return this.httpClient.get<{places: Place[]}>(url)
+    .pipe(
+      map((resData) => resData.places),
+      catchError((error) => {
+        console.log(error)
+          return throwError(() => {
+            return new Error(errorMessage)
+          });
+        })
+      )
+  }
 }
