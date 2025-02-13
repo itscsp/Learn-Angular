5674c142fbca27a4a6389ebe7e7fcd8c5fa102ec
Chethan S Poojary <86185190+itscsp@users.noreply.github.com>
2025-02-13 07:19:51 +0000

    Learning observable and building custom observable

--- 10-rxjs-observables/src/app/app.component.ts
+++ 10-rxjs-observables/src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
+import {interval, map, Observable, subscribeOn} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  clickCount = signal(0)
+  clickCount$ = toObservable(this.clickCount)
+  private distoryRef = inject(DestroyRef);
 
+  constructor() {
+    // effect(() => {
+    //   console.log('Clicked Button', this.clickCount())
+    // })
+
+  }
+
+  customInterval$ = new Observable((subscriber) => {
+    setInterval(() => {
+      console.log('Emitting new values...');
+      subscriber.next({message: 'New value'});
+    }, 2000)
+  });
+
+
+  ngOnInit(): void {
+    // const subscription = interval(1000).pipe(
+    //   map((val) => val * 2 )
+    // ).subscribe({
+    //   next: (val) => console.log('Intevals', val)
+    // });
+
+    
+    const subscription = this.clickCount$.subscribe({
+      next: (val) => console.log('Intevals', val)
+    });
+
+    this.distoryRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+
+    this.customInterval$.subscribe({
+      next: (val) => console.log('ABCD')
+    })
+    
+  }
+
+  onClick() {
+    this.clickCount.update(pervCount => pervCount + 1)
+  }
 }
