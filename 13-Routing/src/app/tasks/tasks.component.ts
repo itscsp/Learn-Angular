84dbb93afc6d68c14278ce59573feaf3bf528662
Chethan S Poojary <86185190+itscsp@users.noreply.github.com>
2025-02-18 05:32:23 +0000

    Adding query param to router like ?order=desc

--- 13-Routing/src/app/tasks/tasks.component.ts
+++ 13-Routing/src/app/tasks/tasks.component.ts
@@ -1,18 +1,44 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, signal } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
 import { TasksService } from './tasks.service';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { __param } from 'tslib';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
+  imports: [TaskComponent, RouterLink],
 })
 export class TasksComponent { 
   userId = input.required<string>();
+  // order = input<'asc' | 'desc'>();
+  order = signal<'asc' | 'desc'>('desc');
+
   private taskService = inject(TasksService);
-  userTasks = computed(() => this.taskService.allTasks().filter(task => task.userId === this.userId()))
+  userTasks = computed(() => 
+    this.taskService.allTasks().filter(task => task.userId === this.userId())
+    .sort((a,b) => {
+      if(this.order() === 'desc') {
+        return a.id > b.id ? -1 : 1;
+      } else {
+        return a.id > b.id ? 1 : -1;
+      }
+    })
+  )
+
+  private activatedRoute = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    const subscription = this.activatedRoute.queryParams.subscribe({
+      next: (params) => (this.order.set(params['order'])),
+    })
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+
 }
