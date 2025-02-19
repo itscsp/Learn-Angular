c517b8f67417e9024118db705dc5ced35ba3aff9
Chethan S Poojary <86185190+itscsp@users.noreply.github.com>
2025-02-19 05:47:54 +0000

    Route lazy loading

---
+++ 14-lazy-loading/src/app/users/users.routes.ts
@@ -0,0 +1,52 @@
+import { ResolveFn, Routes } from '@angular/router';
+import { TasksService } from '../tasks/tasks.service';
+import { Task } from '../tasks/task/task.model';
+import { inject } from '@angular/core';
+import { canLeaveEditPage, NewTaskComponent } from '../tasks/new-task/new-task.component';
+
+
+
+export const resolveUserTasks: ResolveFn<Task[]> = (
+  activatedRouteSnapshot,
+  routerState
+) => {
+  const order = activatedRouteSnapshot.queryParams['order'];
+  const tasksService = inject(TasksService);
+  const tasks = tasksService
+    .allTasks()
+    .filter(
+      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
+    );
+
+  if (order && order === 'asc') {
+    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+  } else {
+    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
+  }
+
+  return tasks.length ? tasks : [];
+};
+
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full',
+  },
+  {
+    path: 'tasks', // <your-domain>/users/<uid>/tasks
+    // component: TasksComponent,
+    loadComponent: () =>
+      import('../tasks/tasks.component').then((mod) => mod.TasksComponent),
+    runGuardsAndResolvers: 'always',
+    resolve: {
+      userTasks: resolveUserTasks,
+    },
+  },
+  {
+    path: 'tasks/new',
+    component: NewTaskComponent,
+    canDeactivate: [canLeaveEditPage]
+  },
+];
